d455051454bcf3f39ca500faaf1b2cc28ca25217
mariusz <mariuszhod@gmial.com>
2023-05-06 14:42:16 +0200

    login and client4

--- public/Js/skrypt2.js
+++ public/Js/skrypt2.js
@@ -1,20 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navTitle = document.getElementById("navTitle")
-    const navUl = document.getElementById("navUl")
-
-    navTitle.addEventListener("click", () => {
-        if(navUl.classList.contains("nav-ul-hidden"))
-        {
-            navUl.classList.remove("nav-ul-hidden")
-        }
-        else
-        {
-            navUl.classList.add("nav-ul-hidden")
-        }
-    })
-})
-
-document.addEventListener("DOMContentLoaded", () => {
     const navTitle = document.getElementById("navService")
     const navUl = document.getElementById("formService")
 
@@ -29,3 +13,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 })
+
